d9142911474ac94908ce8abe173d86538cd3eb52
Sebastian Rodriguez Camilo <segarodriguezcamilo@gmail.com>
2024-04-24 18:57:11 -0500

    Rutas de habitaciones

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
-
+use App\Http\Controllers\HotelController;
+use App\Http\Controllers\HabitacionController;
+use App\Http\Controllers\ReservaController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,5 +29,22 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
+    
+    
+    //Rutas de habitaciones
+    Route::middleware('auth')->group(function () {
+        Route::get('/habitaciones', [HabitacionController::class, 'index'])->name('habitaciones.index');
+        Route::get('/habitaciones/create', [HabitacionController::class, 'create'])->name('habitaciones.create');
+        Route::post('/habitaciones', [HabitacionController::class, 'store'])->name('habitaciones.store');
+        Route::get('/habitaciones/{habitacion}/edit', [HabitacionController::class, 'edit'])->name('habitaciones.edit');
+        Route::put('/habitaciones/{habitacion}', [HabitacionController::class, 'update'])->name('habitaciones.update');
+        Route::delete('/habitaciones/{habitacion}', [HabitacionController::class, 'destroy'])->name('habitaciones.destroy');
+        });
+    
+        
+
+    
 
 require __DIR__.'/auth.php';
